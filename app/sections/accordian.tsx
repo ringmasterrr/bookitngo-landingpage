f2b8e665695c5d1984ab4c5ccfc186c398f6dad5
theringmasterrr <thevagish@gmail.com>
2024-04-16 16:10:12 +0530

    helo

--- app/sections/accordian.tsx
+++ app/sections/accordian.tsx
@@ -1,9 +1,9 @@
-"use client";
+"use client"
 
 import React, { useState } from "react";
 import Image from "next/image";
-import openIcon from "../../public/close.svg";
-import closeIcon from "../../public/open.svg";
+import openIcon from "../public/close.svg";
+import closeIcon from "../public/open.svg";
 
 interface AccordionProps {
   title: string;
@@ -19,31 +19,25 @@ const Accordion = ({ title, answer }: AccordionProps) => {
   };
 
   return (
-    <div className="py-8 border-b border-[#D4D2E3]  xl:w-[98%] w-[80%]">
+    <div className="py-2 border-b border-[#D4D2E3] w-[85rem]">
       <button
         onClick={() => setAccordionOpen(!accordionOpen)}
         className="flex justify-between w-full text-black"
         style={{ fontSize: "20px" }}
       >
-        <span className="text-left lato text-xl font-bold">{title}</span>
+        <span className="text-left font-omnes text-2xl font-extrabold">{title}</span>
         <div style={iconStyle}>
           <Image
             src={accordionOpen ? openIcon : closeIcon}
-            alt={
-              accordionOpen.toString() === "true" ? "Open Icon" : "Close Icon"
-            }
+            alt={accordionOpen ? "Open Icon" : "Close Icon"}
             width={40}
             height={40}
-            layout="intrinsic"
-            loading="eager"
           />
         </div>
       </button>
       <div
-        className={`grid overflow-hidden transition-all duration-300 ease-in-out lato py-6 w-10/12 ${
-          accordionOpen
-            ? "grid-rows-[1fr] opacity-100"
-            : "grid-rows-[0fr] opacity-0"
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out font-omnesreg font-black py-6 w-10/12 ${
+          accordionOpen ? "grid-rows-[1fr] opacity-100" : "grid-rows-[0fr] opacity-0"
         }`}
         style={{ fontSize: "18px" }}
       >
